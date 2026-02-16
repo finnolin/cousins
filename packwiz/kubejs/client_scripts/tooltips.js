f68122ddbb51ebf4de96d9c9504602eff6d8e392
fnnln <mail@fnnln.com>
2026-02-16 14:25:30 +0100

    fix: :bug: fix Kubejs error

---
+++ packwiz/kubejs/client_scripts/tooltips.js
@@ -0,0 +1,6 @@
+ItemEvents.tooltip((event) => {
+  event.add(
+    "eclipticseasons:heat_protective_helmets",
+    Text.gold("Protects from heat stroke"),
+  );
+});
--- packwiz/kubejs/server_scripts/ecliptic_seasons.js
+++ packwiz/kubejs/server_scripts/ecliptic_seasons.js
@@ -26,10 +26,3 @@ ServerEvents.tags("item", (event) => {
     "minecraft:turtle_helmet",
   ]);
 });
-
-ItemEvents.tooltip((event) => {
-  event.add(
-    "eclipticseasons:heat_protective_helmets",
-    Text.gold("Protects from heat stroke"),
-  );
-});
