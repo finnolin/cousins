8d797793646773b38169dd5c00135ef522591720
fnnln <mail@fnnln.com>
2026-02-16 22:06:48 +0100

    feat: Add compat

--- packwiz/kubejs/server_scripts/tfmg_compat.js
+++ packwiz/kubejs/server_scripts/tfmg_compat.js
@@ -1,7 +1,19 @@
 ServerEvents.recipes((event) => {
   event.remove({ output: "#forge:ingots/steel", type: "create:mixing" });
+  event.remove({ output: "#forge:ingots/steel", type: "ad_astra:alloying" });
+  event.remove({ id: "mekanism:processing/steel/enriched_iron_to_dust" });
   event.remove({
-    output: "mekanism:dust_steel",
-    type: "mekanism:metallurgic_infuser",
+    id: "ad_astra:alloying/steel_ingot_from_alloying_iron_ingot_and_coals",
+  });
+  event.remove({ id: "ad_astra:alloying/steel_ingot" });
+
+  event.remove({ output: "mekanism:jetpack" });
+  event.shaped("mekanism:jetpack", ["ABA", "CDC", "EFE"], {
+    A: "mekanism:steel_casing",
+    B: "mekanism:elite_control_circuit",
+    C: "ad_astra:ostrum_engine",
+    D: "mekanism:basic_chemical_tank",
+    E: "tfmg:exhaust",
+    F: "tfmg:plastic_mechanical_pump",
   });
 });
